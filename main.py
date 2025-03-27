3ef88ea93ca59b1f78d7a34ada2895aae63c3ee5
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-03-27 14:47:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ try:
     # 开始循环随机关注用户 (Start looping through random users to follow)
     while True:
         # 生成随机用户 ID (Generate a random user ID)
-        idRandom = random.randint(1500000001, 8162450098)
+        idRandom = random.randint(1500000001, 8214240812)
         print(f"Checking user with ID: {idRandom}")
 
         # 访问用户页面 (Visit the user profile page)
